445bbd507e5facecae7f5e51c0460b14af491c4e
Carter Jacobs <jacobs81@wwu.edu>
2023-11-26 23:11:14 -0800

    Added buttons and there definitions. Not Pretty but functional

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,77 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
-const HomeScreen = () => {
+const App = () => {
+  const [visibleIndex, setVisibleIndex] = useState(null);
+
+  const buttons = [
+    { label: "What is Inflation?", definition: "Inflation is the rate at which the general level of prices for goods and services is rising, and consequently, the value of your money is eroding. In simpler terms, when there is inflation, each dollar you have buys you less than it did before, making it harder to afford the same things you could with the same amount of money in the past." },
+    { label: "An Example of Inflation", definition: "Imagine you have $100 today, and the inflation rate is 2%. If there is 2% inflation over the next year, you would need $102 to purchase the same goods and services that $100 could buy you the previous year. This means that the value of your money has decreased because prices have gone up." },
+    { label: "Why does Inflation happen?", definition: "Inflation happens when the overall demand for goods and services outpaces their supply, leading to a sustained increase in prices. Factors such as increased consumer demand, rising production costs, and disruptions to supply chains contribute to inflation. Additionally, monetary policies, wage-price dynamics, and global influences play significant roles in shaping inflationary trends in an economy." },
+    { label: "How is inflation Measured?", definition: "Inflation is measured by various indices, with the Consumer Price Index (CPI) being a common one. CPI tracks the average change over time in the prices paid by urban consumers for a market basket of consumer goods and services." },
+    { label: "Problems with measuring inflation", definition: "Measuring inflation faces challenges due to the static nature of the basket of goods and services, potentially overlooking shifts in consumer spending habits. Quality changes in products, globalization, and the rapid introduction of new technologies can lead to inaccuracies in tracking price changes over time. Additionally, the fixed weights assigned to items in the inflation basket and the timing of price data collection pose limitations, impacting the precision of inflation measurements." },
+    { label: "How can YOU beat inflation", definition: "If you're living paycheck to paycheck, prioritize building a modest emergency fund to cushion against unexpected expenses. Create a budget focused on essential needs, cutting down on discretionary spending. Explore additional income sources, consider government assistance programs, and manage debt to improve your financial resilience over time." },
+    { label: "Historical Inflation rate for the US", definition: "Since 2008, the United States has generally experienced moderate inflation, with rates staying below 2% for much of the 2010s. The COVID-19 pandemic in 2020 caused fluctuations, and in 2021, inflation temporarily exceeded 5% due to factors like supply chain disruptions. In 2022, inflation remained a monitored concern, with central banks adjusting policies based on economic conditions." },
+  ];
+
+  const handleButtonPress = (index) => {
+    setVisibleIndex(visibleIndex === index ? null : index);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text>Graph 1</Text>
+      <View style={styles.line}></View>
+      <Text>Graph 2</Text>
+      <View style={styles.line}></View>
+      <Text>Graph 3</Text>
+      <View style={styles.line}></View>
+
+      {buttons.map((button, index) => (
+        <View key={index} style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress(index)}
+          >
+            <Text>{button.label}</Text>
+          </TouchableOpacity>
+          {visibleIndex === index && (
+            <View style={styles.definitionContainer}>
+              <Text>{button.definition}</Text>
+            </View>
+          )}
+        </View>
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    width: 300,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    margin: 10,
     padding: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginBottom: 5, // Add margin to separate each button and definition
+  },
+  button: {
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#DDDDDD",
+  },
+  definitionContainer: {
+    padding: 10,
+    margin: 5,
+    border: "1px solid #CCCCCC",
+  },
+  line: {
+    borderBottomColor: "black",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    width: "80%", 
+    alignSelf: "center", 
+    marginVertical: 10, 
   },
 });
 
-export default HomeScreen;
+export default App;
