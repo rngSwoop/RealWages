5771c90af0241a790a97d406769a612528dddbc1
Garrett <garrettkingmusic@gmail.com>
2023-12-12 20:26:13 -0800

    removed warning notification

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator.js';
 import { UserProvider } from './src/assets/UserContext';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const App = () => {
   return (
